368b678c16b80f75b057deea2fd8b82fa90d31e5
RonaldChicaVera <rchica@rootstack.com>
2022-03-11 14:21:07 -0500

    isadmin field dor user

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -20,6 +20,7 @@ class AdminSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'saldo' => 0,
+            'is_admin' => 1,
         ])->assignRole('writer', 'admin');
     }
 }
